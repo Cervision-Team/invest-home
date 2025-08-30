26a3aa51d0c1624b5504d1cd42a100d50df96a41
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2025-08-30 13:34:23 +0400

    reconfigured layout of authentication

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,126 +1,39 @@
-"use client";
-
-import { useForm, Controller } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { useRouter } from "next/navigation";
-import { loginWithPhone } from "@/lib/authService";
-import { toast } from "react-toastify";
-import Image from "next/image";
-import AuthLayout from "@/app/(auth)/AuthLayout";
-import Google from "../../../../public/icons/google.svg"
-import Phone from "../../../../public/icons/phone-auth.svg"
-import Link from "next/link";
-
-const globalPhoneRegex = /^\+?[1-9]\d{7,14}$/;
-
-const schema = yup.object({
-  phone: yup
-    .string()
-    .required("Telefon nömrəsi vacibdir")
-    .matches(globalPhoneRegex, "Telefon nömrəsi düzgün formatda deyil"),
-});
+import EntryGate from "../components/EntryGate";
+import LoginForm from "../components/login/LoginForm";
 
 const LoginPage = () => {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) });
-
-  const router = useRouter();
-
-  const onSubmit = async (data) => {
-    try {
-      const result = await loginWithPhone(data.phone);
-      localStorage.setItem("phone", data.phone);
-      localStorage.setItem("entranceType", "LOGIN");
-      router.replace("/otp");
-    } catch (error) {
-      if (error.response?.status === 404) {
-        toast.error(error.response.data.message || "İstifadəçi tapılmadı");
-      } else {
-        toast.error(error.response?.data?.message || "Xəta baş verdi");
-      }
-    }
-  };
 
   return (
-    <AuthLayout>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full">
-        <div className="flex flex-col items-center gap-8 pb-8">
-          <h1 className="text-4xl font-medium text-1">Daxil ol</h1>
-          <p className="text-lg text-1 text-center">
-            "Invest <span className="text-[var(--color-primary)]">Home</span> – Gələcəyin evini bu gün seç, rahatlığını sabahdan
-            yaşa!"
-          </p>
-        </div>
+    <>
+      <section className="relative h-screen">
+        <div className="flex h-full">
+          <div className="w-[50%]">
 
-        <div className="flex flex-col gap-1 mb-[28px]">
-          <label htmlFor="phone" className="text-sm font-medium text-black">
-            Telefon<span className="text-red-500">*</span>
-          </label>
-          <div className="relative w-full">
-            <Controller
-              name="phone"
-              control={control}
-              defaultValue="+994"
-              render={({ field }) => (
-                <input
-                  {...field}
-                  type="tel"
-                  id="phone"
-                  placeholder="994*********"
-                  className="h-[44px] px-4 border border-black w-full text-base rounded-lg"
-                />
-              )}
-            />
-            <span className="absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none">
-              <Image
-                src={Phone.src}
-                alt="phone"
-                width={20}
-                height={20}
-              />
-            </span>
           </div>
-          <div className="h-[28px] text-sm text-red-500">
-            {errors.phone?.message && <span>{errors.phone.message}</span>}
+          <div className="bg-[#02836F] w-[50%]">
+
           </div>
         </div>
-
-        <div className="flex flex-col gap-6">
-          <Link href="../otp">
-            <button
-              type="submit"
-              className="w-full bg-primary text-white py-3 px-6 rounded-lg hover:opacity-90"
-            >
-              Daxil ol
-            </button>
-          </Link>
-
-
-          <div className="flex items-center justify-center gap-4">
-            <span className="h-px w-full bg-[#866AB4]" />
-            <span className="text-sm text-1 whitespace-nowrap">və ya</span>
-            <span className="h-px w-full bg-[#866AB4]" />
+        <div className="max-w-[1600px] mx-auto absolute top-0 left-[50%] translate-x-[-50%] h-full w-full flex items-center">
+          <div className="w-[50%] px-[80px] max-[1025px]:px-[20px] max-[431px]:px-[16px] flex justify-center">
+            <div className="w-[410px] flex flex-col gap-[36px]">
+              <div className="flex flex-col gap-[24px]">
+                <h1 className="text-[32px] font-medium text-center">Daxil ol</h1>
+                <p className="text-lg text-center">
+                  "Invest <span className="text-[var(--color-primary)]">Home</span> – Gələcəyin evini bu gün seç, rahatlığını sabahdan
+                  yaşa!"
+                </p>
+              </div>
+              <LoginForm />
+            </div>
           </div>
-
-          <button
-            type="button"
-            className="w-full flex items-center justify-center gap-2 border border-primary text-[#1B1F27] py-3 rounded-lg hover:bg-green-50"
-          >
-            <Image
-              src={Google.src}
-              alt="Google"
-              width={20}
-              height={20}
-            />
-            <span className="font-medium">Sign in with Google</span>
-          </button>
+          <div className="w-[50%] px-[80px] max-[1025px]:px-[20px] max-[431px]:px-[16px]">
+            <EntryGate />
+          </div>
         </div>
-      </form>
-    </AuthLayout>
+      </section>
+
+    </>
   );
 };
 
--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -14,7 +14,6 @@ export default function Layout({ children }) {
                 <Header />
                 {children}
                 <TabBar />
-                
                 <Footer />
                 <SubFooter />
                
