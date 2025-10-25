d5d3de9fa74971c821b3417385badeb625ab1e63
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2025-10-25 09:54:31 +0400

    fixed blogs carousel responsiveness

--- src/app/(root)/Home/Blogs.jsx
+++ src/app/(root)/Home/Blogs.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
-import { blogData } from "../../../components/core/BlogsData"; 
+import { blogData } from "../../../components/core/BlogsData";
 import BlogCard from "@/components/ui/BlogCard";
 
 const Blogs = () => {
@@ -49,44 +49,46 @@ const Blogs = () => {
 
   return (
     <section className="max-[431px]:hidden [background:linear-gradient(180deg,_#02836F_0%,_#001D19_100%)]">
-      <div className="w-full mx-auto items-center pt-[100px] px-[80px] max-[1025px]:px-[20px] pb-[210px]">
-        <p className="text-white text-[19.4px] font-medium tracking-[2.91px] uppercase text-center">
-          Trenddə nədir ?
-        </p>
-        <h2 className="mb-[40px] text-white text-[39.8px]/[47px] font-semibold mt-[30px] text-center">
-          Ən son Bloqlar və Yazılar
-        </h2>
+      <div className="max-w-[1600px] mx-auto">
+        <div className="w-full mx-auto items-center pt-[100px] px-[80px] max-[1025px]:px-[20px] pb-[210px]">
+          <p className="text-white text-[19.4px] font-medium tracking-[2.91px] uppercase text-center">
+            Trenddə nədir ?
+          </p>
+          <h2 className="mb-[40px] text-white text-[39.8px]/[47px] font-semibold mt-[30px] text-center">
+            Ən son Bloqlar və Yazılar
+          </h2>
 
-        <div className="flex flex-row">
-          <Swiper
-            modules={[Autoplay]}
-            loop
-            autoplay={{
-              delay: 3000,
-              disableOnInteraction: false,
-            }}
-            slidesPerView={1}
-            speed={500}
-            spaceBetween={20}
-            className="w-full blog-swiper"
-            breakpoints={{
-              425: { slidesPerView: 1, spaceBetween: 15 },
-              450: { slidesPerView: 2, spaceBetween: 20 },
-              1024: { slidesPerView: 3, spaceBetween: 20 },
-            }}
-          >
-            {blogs.map((data, index) => (
-              <SwiperSlide key={index}>
-                <BlogCard
-                  image={data.image}
-                  title={data.title}
-                  description={data.description}
-                  day={data.day}
-                  month={data.month}
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+          <div className="flex flex-row">
+            <Swiper
+              modules={[Autoplay]}
+              loop
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+              }}
+              slidesPerView={1}
+              speed={500}
+              spaceBetween={20}
+              className="w-full blog-swiper"
+              breakpoints={{
+                550: { slidesPerView: 1, spaceBetween: 20 },
+                768: { slidesPerView: 2, spaceBetween: 20 },
+                1024: { slidesPerView: 3, spaceBetween: 20 },
+              }}
+            >
+              {blogs.map((data, index) => (
+                <SwiperSlide key={index}>
+                  <BlogCard
+                    image={data.image}
+                    title={data.title}
+                    description={data.description}
+                    day={data.day}
+                    month={data.month}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </div>
       </div>
     </section>
