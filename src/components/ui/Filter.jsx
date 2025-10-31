297e4a15d6b7deb798d7828eac0c9b377f8f0a61
ZaurBagishov <bagisovzaur6@gmail.com>
2025-10-30 21:48:42 -0700

    improvements on filter.jsx

--- src/components/ui/Filter.jsx
+++ src/components/ui/Filter.jsx
@@ -139,7 +139,6 @@ const ADDITIONAL_FILTERS = [
   }
 ];
 
-// FilterBtn Component
 const FilterBtn = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
     <path
